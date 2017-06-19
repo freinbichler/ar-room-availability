31d6a339eb8bb27222a7283acfe5202167f66216
Marcel Freinbichler <marcel@freinbichler.me>
2017-06-19 18:01:57 +0200

    feat: add rotation overlay

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import Roomplan from './components/roomplan';
 import ARJSConfig from './components/arjs-config';
+import RotateDevice from './components/rotate-device';
 
 const arjsConfig = new ARJSConfig();
+const rotateDevice = new RotateDevice();
 const roomplan = new Roomplan();
---
+++ src/js/components/rotate-device.js
@@ -0,0 +1,17 @@
+export default class RotateDevice {
+  constructor() {
+    this.overlay = document.querySelector('.overlay-rotate-device');
+
+    this.addEventListener().bind(this);
+  }
+
+  addEventListener() {
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > window.innerHeight) {
+        this.overlay.style.display = 'none';
+      } else {
+        this.overlay.style.display = 'flex';
+      }
+    });
+  }
+}
