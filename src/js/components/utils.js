17fe72b824e88bd83f4a5ca8e0331da3f9442ba7
Marcel Freinbichler <marcel@freinbichler.me>
2017-06-19 16:44:15 +0200

    feat: calculate time until room is free again

--- src/js/components/utils.js
+++ src/js/components/utils.js
@@ -46,23 +46,56 @@ export function calculateAvailabilityDuration(free, roomActivities, index) {
   console.log(free);
   console.log(roomActivities);
   console.log(index);
-  let duration;
+  let duration = 'all day';
+  const now = window.moment();
+
   if (free) {
-    if (roomActivities.length - 1 > index) {
+    // if room is currently free, calculate time until next activity
+    if (roomActivities.length > 0) {
       // time to next activity
-      duration = window.moment.range(
-        window.moment(),
-        window.moment(roomActivities[index + 1].begin),
-      );
-    } else {
-      // no activities left today
-      duration = 'all day';
+      for (let activity of roomActivities) {
+        if (now.isBefore(activity.begin)) {
+          duration = window.moment.range(
+            now,
+            window.moment(activity.begin),
+          );
+          break;
+        }
+      }
     }
   } else {
-    // TODO: wenn besetzt
+    // if room is currently occupied, calculate time until the room is free again (for > 15 minutes)
+    const currentActivity = roomActivities[index];
+    if (index < roomActivities.length - 1) {
+      for (let i = index; i < roomActivities.length - 1; i++) {
+        const range = window.moment.range(
+          window.moment(roomActivities[i].end),
+          window.moment(roomActivities[i + 1].begin),
+        );
+        if (range.diff('minutes') > 15) {
+          // time until end of activity block (including breaks)
+          duration = window.moment.range(
+            now,
+            window.moment(roomActivities[i].end),
+          );
+          break;
+        } else if (i === roomActivities.length - 2) {
+          // time until last activity of the day ends if end day ends with activity block
+          duration = window.moment.range(
+            now,
+            window.moment(roomActivities[i + 1].end),
+          );
+        }
+      }
+    } else {
+      duration = window.moment.range(
+        now,
+        window.moment(currentActivity.end),
+      );
+    }
   }
 
-  const humanizedDuration = (typeof duration === 'string') ? 'all day' : humanizeDuration(duration + 0, { units: ['h', 'm'], round: true });
+  const humanizedDuration = (typeof duration === 'string') ? duration : humanizeDuration(duration + 0, { units: ['h', 'm'], round: true });
   return { duration, humanizedDuration };
 }
 
@@ -70,7 +103,12 @@ export function calculateAvailability(roomActivities) {
   const now = window.moment();
   let free = true;
   let index = -1;
-
+  // const roomActivities = [
+  //   { begin: '2017-06-19 15:00:00', end: '2017-06-19 15:30:00' },
+  //   { begin: '2017-06-19 16:00:00', end: '2017-06-19 16:30:00' },
+  //   { begin: '2017-06-19 17:00:00', end: '2017-06-19 17:30:00' },
+  //   { begin: '2017-06-19 18:00:00', end: '2017-06-19 18:30:00' },
+  // ];
   roomActivities.map((activity, i) => {
     if (free) {
       // check if now is withing an activity
